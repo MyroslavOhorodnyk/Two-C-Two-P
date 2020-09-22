24a5f0a42794445d75a6398d226aab18a97f2b55
Myroslav Ohorodnyk <ohorodnyk95@gmail.com>
2020-09-22 18:58:33 +0300

    Added script to create DB and Transactions table

---
+++ Database.sql
@@ -0,0 +1,26 @@
+﻿IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '2c2p')
+  BEGIN
+    CREATE DATABASE [2c2p]
+  END
+    
+GO
+    USE [2c2p]
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Transactions' and xtype='U')
+BEGIN
+    CREATE TABLE Transactions (
+        [Id] [uniqueidentifier] NOT NULL,
+        [Ammount] [decimal](19, 4) NOT NULL,
+		[Currency] [nvarchar](50) NOT NULL,
+		[TransactionDate] [datetimeoffset](7) NOT NULL,
+		[TransactionStatus] [nvarchar](50) NOT NULL,
+		[Status] [tinyint] NOT NULL,
+		[CreatedBy] [nvarchar](50) NOT NULL,
+		[ModifiedBy] [nvarchar](50) NOT NULL,
+		[CreatedDate] [datetimeoffset](7) NOT NULL,
+		[ModifiedDate] [datetimeoffset](7) NOT NULL,
+    )
+END
+
+DROP TABLE Transactions
