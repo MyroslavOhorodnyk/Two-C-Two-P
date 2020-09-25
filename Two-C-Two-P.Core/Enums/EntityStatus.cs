df54a53dff0a169568f7a136234b5e8c7ae53afc
Myroslav Ohorodnyk <ohorodnyk95@gmail.com>
2020-09-25 20:23:53 +0300

    Added transaction handling to UnitOfWork

---
+++ Two-C-Two-P.Core/Enums/EntityStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Two_C_Two_P.Core.Enums
+{
+    [Flags]
+    public enum EntityStatus : byte
+    {
+        Undefined = 0,
+        Active = 1,
+        Inactive = 2,
+        Deleted = 4
+    }
+}
--- Two-C-Two-P.Core/Interfaces/IUnitOfWork.cs
+++ Two-C-Two-P.Core/Interfaces/IUnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Two_C_Two_P.Core.Interfaces.Repositories;
 
 namespace Two_C_Two_P.Core.Interfaces
@@ -6,5 +7,9 @@ namespace Two_C_Two_P.Core.Interfaces
     public interface IUnitOfWork : IDisposable
     {
         ITransactionRepository TransactionRepository { get; }
+
+        Task CommitAsync(bool useTransaction = false);
+
+        Task RollbackAsync();
     }
 }
